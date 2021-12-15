37103996ddb5aec904420c8677a8127d8bee6a3c
Truc Truong <truc.truong@kesko.fi>
2021-12-15 22:29:06 +0200

    Initial Commit

---
+++ checklist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checklist
+//
+//  Created by TRUC TRUONG on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
